cf450bfe9dfd7303e5ce0bf804b3403cec26e105
joelssk <joelssk@live.com>
2016-08-10 12:18:20 -0400

    Commenting and Code Cleanup

--- class.rover.php
+++ class.rover.php
@@ -1,18 +1,20 @@
 <?php
+// Example PHP class for a NASA rover
+// Joel McCauley - 8/10/16 - For Level Interactive
 
 class rover {
 	public $x = null;
 	public $y = null;
 	public $direction = null;
 
-
+	// Sets the starting position of the rover
 	public function setPos($x, $y, $direction) {
 		$this->x = $x;
 		$this->y = $y;
 		$this->direction = $direction;
 	}
 
-
+	// Parses the individual letter commands into an array via str_split, then passes them to the command handler
 	public function command($command) {
 		$command = str_split($command);
 			foreach ($command as $data) {
@@ -20,41 +22,45 @@ class rover {
 			}
 	}
 
-
+	// Outputs the finalized string to buffer
 	public function outputPos() {
 		echo $this->x." ".$this->y." ".$this->direction;
 	}
 
-
+	// Issues the command to proper handler
 	private function handler($data) {
 		switch ($data) {
 	    case 'L': $this->left(); break;
 			case 'R': $this->right(); break;
 			case 'M': $this->move(); break;
+			default: $this->outputError("Error in handler");
 		}
 	}
 
-
+	// Move - The below comment represents the movement of N/S/E/W on an x/y axis
+	// N=+1, E=+1, S=-1, W=-1
 	private function move() {
 		switch ($this->direction) {
 			case 'N': $this->y++; break;
 			case 'E': $this->x++; break;
 			case 'S': $this->y--; break;
 			case 'W': $this->x--; break;
+			default: $this->outputError("Error in move handler");
 		}
 	}
 
-
+	// Left - Handles the rovers movement in the left direction
 	private function left() {
 		switch ($this->direction) {
 	    case 'N': $this->direction = 'W'; break;
 	    case 'W': $this->direction = 'S'; break;
 	    case 'S': $this->direction = 'E'; break;
 	    case 'E': $this->direction = 'N'; break;
+			default: $this->outputError("Error in left handler");
 		}
 	}
 
-
+	// Right - Handles the rovers movement in the right direction
 	private function right() {
 		switch ($this->direction) {
 	    case 'N': $this->direction = 'E'; break;
@@ -65,7 +71,7 @@ class rover {
 		}
 	}
 
-
+	// Returns a failure message to the buffer
 	private function outputError($message) {
 		echo $message;
 	}
--- rover.php
+++ rover.php
@@ -1,10 +1,24 @@
 <?php
+// Example PHP class usage for a NASA rover
+// Joel McCauley - 8/10/16 - For Level Interactive
+// Would liked to have made the output more readable, but wasn't sure if I could echo out HTML.
+
+// Require rover class
 require('class.rover.php');
 
+// Load rover class
 $rover = new rover();
+
+// Set starting position rover #1
 $rover->setPos(1, 2, 'N');
+
+// Issue a command to rover #1
 $rover->command('LMLMLMLMM');
+
+// Output final location rover #1
 $rover->outputPos();
+
+// Rover #2
 $rover->setPos(3, 3, 'E');
 $rover->command('MMRMMRMRRM');
 $rover->outputPos();
